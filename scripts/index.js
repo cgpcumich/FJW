b4d452dcd58e712246cf57b90638d497d2f4ab1c
Collin Cavanaugh <cgpc@umich.edu>
2022-11-18 02:38:52 -0500

    UI Change

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
+const loggedOutLinks = document.querySelectorAll('.logged-out');
+const loggedInLinks = document.querySelectorAll('.logged-in');
+//const accountDetails = document.querySelector('.account-details');
+//const adminItems = document.querySelectorAll('.admin');
+
+const setupUI = (user) => {
+  if (user) {
+    // toggle user UI elements
+    loggedInLinks.forEach(item => item.style.display = 'block');
+    loggedOutLinks.forEach(item => item.style.display = 'none');
+  } else {
+    // toggle user elements
+    loggedInLinks.forEach(item => item.style.display = 'none');
+    loggedOutLinks.forEach(item => item.style.display = 'block');
+  }
+};
 // setup materialize components
 document.addEventListener('DOMContentLoaded', function() {
 
